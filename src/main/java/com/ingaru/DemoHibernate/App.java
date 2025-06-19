62f26ab7f2a406e42b75ebdc181d7d5d5a466c15
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-06-19 11:33:36 -0400

    first commit

---
+++ src/main/java/com/ingaru/DemoHibernate/App.java
@@ -0,0 +1,46 @@
+	package com.ingaru.DemoHibernate;
+	
+	import com.ingaru.DemoHibernate.Student;
+	import org.hibernate.Session;
+	import org.hibernate.SessionFactory;
+	import org.hibernate.Transaction;
+	import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+	
+	/**
+	 * Hello world!
+	 *
+	 */
+	public class App 
+	{
+	    public static void main( String[] args )
+	    {
+	    	Student s = null;
+	        //Student student = new Student(103,"Carolina");
+	        
+	        Configuration config = new Configuration();
+	        //Configuration config = new Configuration().configure().addAnnotatedClass(Student.class);
+	        //ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(config.getProperties()).buildServiceRegistry();
+	        config.configure("hibernate.cfg.xml");
+	        SessionFactory factory = config.buildSessionFactory();
+	        Session session = factory.openSession();
+	        //Transaction tx = session.beginTransaction();
+	        session.beginTransaction();
+	        //session.save(student);
+	        
+	        s= (Student) session.get(Student.class, 101);
+	        System.out.println(s);
+	        s= (Student) session.get(Student.class, 101);
+	        System.out.println(s);
+	        
+	        
+	        //tx.commit();
+	        session.getTransaction().commit();
+	        session.close();
+	        System.out.println("Record Saved Succesfully");
+	        
+	        
+	        
+	        
+	    }
+	}
---
+++ src/main/java/com/ingaru/DemoHibernate/Student.java
@@ -0,0 +1,42 @@
+package com.ingaru.DemoHibernate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	private int rollno;
+	private String sname;
+	
+	public Student() {
+		
+	}
+	
+	public Student(int rollno, String sname) {
+		super();
+		this.rollno = rollno;
+		this.sname = sname;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", sname=" + sname + "]";
+	}
+	
+	
+
+}
